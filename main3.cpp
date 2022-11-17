937094f3bd66569d06f5fd1c43b8f68e821c5142
Vlad Filonenko <75538640+fl1nK@users.noreply.github.com>
2022-11-17 03:34:50 +0200

    added for lr5

---
+++ main3.cpp
@@ -0,0 +1,112 @@
+﻿'Project1.exe' (Win32): Loaded 'D:\KPI_3_1\Graphics\Project1\x64\Debug\Project1.exe'. Symbols loaded.
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ntdll.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\kernel32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\KernelBase.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\user32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\win32u.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\gdi32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\gdi32full.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\msvcp_win.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ucrtbase.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\shell32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\vulkan-1.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\cfgmgr32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\advapi32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\msvcrt.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\sechost.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\rpcrt4.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\msvcp140d.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140d.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140_1d.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ucrtbased.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\imm32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\dxgi.dll'. 
+The thread 0x3e78 has exited with code 0 (0x0).
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\uxtheme.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\combase.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\dinput8.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\XInput1_4.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\devobj.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\dwmapi.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\SHCore.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\InputHost.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\oleaut32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\WinTypes.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\propsys.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\CoreUIComponents.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\bcryptprimitives.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ntmarta.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\kernel.appcore.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\CoreMessaging.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ws2_32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\msctf.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\hid.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\setupapi.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\bcrypt.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\winmm.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\wintrust.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\crypt32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\msasn1.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\TextInputFramework.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ResourcePolicyClient.dll'. 
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\ResourcePolicyClient.dll'
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DriverStore\FileRepository\nv_dispi.inf_amd64_1b5e80ff87b1f5c8\nvoglv64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\ole32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\wtsapi32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\version.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\cryptbase.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\cryptnet.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\drvstore.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\wldp.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igvk64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\shlwapi.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\opengl32.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\glu32.dll'. 
+The thread 0x23ec has exited with code 0 (0x0).
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igdgmm64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igc64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igdmd64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\clbcatq.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\VulkanSDK\1.3.224.1\Bin\VkLayer_khronos_validation.dll'. Symbols loaded.
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\msvcp140.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\vcruntime140_1.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\windows.storage.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DXCore.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\nvspcap64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\profapi.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\nvapi64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\imagehlp.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\cryptsp.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\rsaenh.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\gpapi.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\DriverStore\FileRepository\nv_dispi.inf_amd64_1b5e80ff87b1f5c8\NvCamera\NvCameraAllowlisting64.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\AppXDeploymentClient.dll'. 
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\AppXDeploymentClient.dll'
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\powrprof.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\umpdc.dll'. 
+'Project1.exe' (Win32): Loaded 'C:\Windows\System32\winsta.dll'. 
+The thread 0xb70 has exited with code 0 (0x0).
+The thread 0x33b4 has exited with code 0 (0x0).
+The thread 0x4fdc has exited with code 0 (0x0).
+The thread 0x4dc8 has exited with code 0 (0x0).
+The thread 0x1f7c has exited with code 0 (0x0).
+The thread 0x4d58 has exited with code 0 (0x0).
+The thread 0x2b80 has exited with code 0 (0x0).
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\vcruntime140_1.dll'
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\msvcp140.dll'
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\vcruntime140.dll'
+'Project1.exe' (Win32): Unloaded 'C:\VulkanSDK\1.3.224.1\Bin\VkLayer_khronos_validation.dll'
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igdmd64.dll'
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igc64.dll'
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igdgmm64.dll'
+'Project1.exe' (Win32): Unloaded 'C:\Windows\System32\DriverStore\FileRepository\iigd_dch.inf_amd64_51f685305808e3a5\igvk64.dll'
+The thread 0x4f5c has exited with code 0 (0x0).
+The thread 0x4530 has exited with code 0 (0x0).
+The thread 0x4754 has exited with code 0 (0x0).
+The thread 0x4e98 has exited with code 0 (0x0).
+The thread 0x4310 has exited with code 0 (0x0).
+The thread 0x45c0 has exited with code 0 (0x0).
+The thread 0x19a0 has exited with code 0 (0x0).
+The thread 0x4884 has exited with code 0 (0x0).
+The program '[19044] Project1.exe' has exited with code 0 (0x0).
